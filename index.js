64acbd0f4dac45b9c5dc12aad088ecad0031df26
Austin <austinodhiambo64@gmail.com>
2023-03-13 15:40:43 +0300

    Done assignment

---
+++ index.js
@@ -0,0 +1,77 @@
+function add() {
+    return "something";
+}
+
+function subtract(a, b) {
+    return "something";
+}
+function multiply(a, b) {
+    return "something";
+  }
+
+  function divide(a, b) {
+    return "something";
+  }
+
+  function add() {
+
+  }
+  function add() {
+    // we'll fill this in shortly
+  }
+  function subtract() {
+    // we'll fill this in shortly
+  }
+  function multiply() {
+    // we'll fill this in shortly
+  }
+  function divide() {
+    // we'll fill this in shortly
+  }
+
+  function add(a, b) {
+    return a + b;
+  }
+
+  function subtract(a, b) {
+    return a - b;
+  }
+
+  function multiply(a, b) {
+    return a * b;
+  }
+
+  function divide(a, b) {
+    return a / b;
+  }
+ 
+//   let number = 5;
+
+// number++; //=> 5... hmmmm
+
+// number; //=> 6 -- the number was incremented after it was evaluated
+
+// number--; //=> 6
+
+// number; //=> 5
+
+function increment(n){
+    return n + 1;
+  }
+  function decrement(n){
+    return n - 1;
+  }
+  function makeInt(string){
+    return string;
+  }
+  function preserveDecimal(string){
+    return string;
+  } 
+  function makeInt(n){
+    return parseInt (n, 10);
+  }
+
+  function preserveDecimal(n){
+    return parseFloat (n, 10);
+  } 
+  
